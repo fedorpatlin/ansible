5eef41cf3d0680367db47471ebedc446836310fc
Fyodor Patlin <patlin.f@rusgis.com>
2017-04-14 18:17:45 +0500

    module started

--- lib/ansible/modules/web_infrastructure/wildfly/wildfly_driver.py
+++ lib/ansible/modules/web_infrastructure/wildfly/wildfly_driver.py
@@ -1,21 +1,39 @@
-#!/bin/env python
+#!/usr/bin/env python
 # -*- coding: UTF-8 -*-
+"""
+Module creates jdbc driver using wildfly's http api
+"""
 import json
 import requests
 from requests.auth import HTTPBasicAuth
 from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.basic import get_exception
+from ansible.module_utils.urls import open_url
 
-DOCUMENTATION="""
+DOCUMENTATION = """
+url - management url
+user - management user
+password - management user password
 """
 
-EXAMPLES="""
+EXAMPLES = """
 """
 
 def main():
     """Module init"""
     module = AnsibleModule(argument_spec=dict(
-        port=dict(default=9990)
+        url=dict(default='http://localhost:9990/management'),
+        user=dict(required=True),
+        password=dict(required=True, no_log=True),
     ))
+    url = module.params.get('url')
+    user = module.params.get('user')
+    password = module.params.get('password')
+    try:
+        open_url(url, url_username=user, url_password=password, timeout=3)
+    except Exception:
+        ex = get_exception()
+        module.fail_json(msg="{}".format(type(ex)))
     module.exit_json(changed=False)
 
 if __name__ == '__main__':
